fa53ffeb59e0c97fb4ad34093775e288fff9697c
Praveen Ravichandran <me@pravinba9495.dev>
2020-10-10 03:38:09 -0400

    More API Changes

--- api/src/endpoints/movies.js
+++ api/src/endpoints/movies.js
@@ -1,9 +1,27 @@
-const db = require('../utils/db');
-const {walk} = require('../utils/walk');
+const { getMoviesListFromDb, refreshList, saveMovieInDb } = require('../utils/db');
 const router = require('express').Router();
 
 router.get('/', async (request, response) => {
-	response.send({ data: await walk('/movies') });
+	try {
+		const data = await getMoviesListFromDb();
+		response.send({ data });
+	} catch (error) {
+		console.error(error);
+		response.status(500).send({ error });
+	}
+});
+
+router.get('/refresh', async (request, response) => {
+	try {
+		const data = await refreshList('movies');
+		await Promise.all(data.map((movie) => {
+			return saveMovieInDb(movie);
+		}));
+		response.send({ data });
+	} catch (error) {
+		console.error(error);
+		response.status(500).send({ error });
+	}
 });
 
 module.exports = router;
